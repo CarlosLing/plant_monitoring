23f8b942f0a017925b2186d1bca49c03ba3bb8de
CarlosLing <carlos.gling@hotmail.com>
2023-04-23 09:12:16 +0200

    add None case to function

--- src/sensor_data/utils.py
+++ src/sensor_data/utils.py
@@ -17,6 +17,18 @@ def generate_random_reading(
     min_value: float = 0,
     date_time: Optional[datetime] = None,
 ) -> SensorReadings:
+    """
+    Creates a random reading for the sensor provided
+    input:
+        - sensor:
+        - max/min_value: range of values for the sensor value
+        - date_time: datetime of the reading, if None current time is taken.
+    """
     value = random_value(max_value, min_value)
+
+    if date_time is None:
+        date_time = datetime.now()
+
     sensor_reading = SensorReadings(sensor, value, date_time)
+
     return sensor_reading
