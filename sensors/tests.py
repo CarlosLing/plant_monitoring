10ebea14785e5afcb85a98bba0ba77655efb3362
CarlosLing <carlos.gling@hotmail.com>
2023-04-26 09:49:59 +0200

    Add test for view

--- sensors/tests.py
+++ sensors/tests.py
@@ -1,3 +1,18 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class SensorListViewTest(TestCase):
+    def test_no_sensors(self):
+        response = self.client.get(reverse("sensors:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No sensors are available.")
+        self.assertQuerySetEqual(response.context["sensor_list"], [])
+
+
+# class SensorDetailViewTest(TestCase):
+
+
+# class SensorDetailViewTest(TestCase):
